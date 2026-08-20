107f719786ead91901fd763ea0560218a24d544e
Dxnny2504 <r.guse858@gmail.com>
2026-08-20 23:10:48 +0200

    Refactor Minecraft bot initialization and logging

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-console.log("");
-console.log("========================================");
-console.log("        AFK MINECRAFT TEST");
-console.log("========================================");
-console.log("");
-console.log("[MC] index.js wurde gestartet.");
-console.log("[MC] Node Version:", process.version);
-console.log("[MC] Arbeitsverzeichnis:", process.cwd());
-
 const mineflayer = require("mineflayer");
-
-console.log("[MC] Mineflayer wurde geladen.");
 
 const {
   pathfinder,
@@ -17,76 +6,319 @@ const {
   goals
 } = require("mineflayer-pathfinder");
 
-console.log("[MC] Pathfinder wurde geladen.");
+console.log("");
+console.log("========================================");
+console.log("        GRIEFERGAMES AFK BOT");
+console.log("========================================");
+console.log("");
 
-const bot = mineflayer.createBot({
-  host: "griefergames.net",
-  port: 25565,
-  username: "r.guse858@gmail.com",
-  auth: "microsoft",
-  version: "1.8.9",
-  profilesFolder: "./minecraft-auth"
-});
+console.log("[MC] Node Version:", process.version);
+console.log("[MC] Starte Mineflayer...");
+console.log("[MC] Server: griefergames.net");
+console.log("[MC] Version: 1.8.9");
+console.log("[MC] Benutzer:", process.env.MC_USERNAME || "nicht gesetzt");
+console.log("");
 
-console.log("[MC] Minecraft Bot Objekt wurde erstellt.");
+let bot;
 
-bot.loadPlugin(pathfinder);
+try {
+  console.log("[MC] Erstelle Minecraft Bot...");
 
-console.log("[MC] Pathfinder Plugin geladen.");
+  bot = mineflayer.createBot({
+    host: "griefergames.net",
+    port: 25565,
 
-bot.once("login", () => {
+    username:
+      process.env.MC_USERNAME ||
+      "r.guse858@gmail.com",
+
+    auth: "microsoft",
+
+    version: "1.8.9",
+
+    profilesFolder:
+      "./minecraft-auth"
+  });
+
+  console.log(
+    "[MC] createBot() wurde erfolgreich ausgeführt."
+  );
+
+} catch (error) {
+
   console.log("");
   console.log("========================================");
-  console.log("        MINECRAFT LOGIN");
+  console.log("        CREATE BOT FEHLER");
   console.log("========================================");
   console.log("");
 
-  console.log("[MC] Login erfolgreich.");
-});
+  console.error(error);
 
-bot.once("spawn", () => {
-  console.log("");
-  console.log("========================================");
-  console.log("        MINECRAFT SPAWN");
-  console.log("========================================");
-  console.log("");
+  process.exit(1);
+}
 
-  console.log(
-    "[MC] Position:",
-    bot.entity.position
-  );
+console.log(
+  "[MC] Registriere Minecraft Events..."
+);
 
-  console.log(
-    "[MC] AFK Test erfolgreich."
-  );
-});
+bot.loadPlugin(
+  pathfinder
+);
 
-bot.on("messagestr", message => {
-  console.log(
-    "[CHAT]",
-    message
-  );
-});
+console.log(
+  "[MC] Pathfinder Plugin geladen."
+);
 
-bot.on("kicked", reason => {
-  console.log(
-    "[MC] Gekickt:",
-    reason
-  );
-});
+console.log(
+  "[MC] Warte auf Minecraft Login..."
+);
 
-bot.on("error", error => {
-  console.log(
-    "[MC] FEHLER:"
-  );
+bot.once(
+  "login",
+  () => {
 
-  console.log(
-    error
-  );
-});
+    console.log("");
+    console.log("========================================");
+    console.log("        MINECRAFT LOGIN");
+    console.log("========================================");
+    console.log("");
 
-bot.on("end", () => {
-  console.log(
-    "[MC] Verbindung beendet."
-  );
-});
+    console.log(
+      "[MC] Login erfolgreich."
+    );
+
+    console.log(
+      "[MC] Username:",
+      bot.username
+    );
+
+    console.log(
+      "[MC] Version:",
+      bot.version
+    );
+  }
+);
+
+bot.once(
+  "spawn",
+  () => {
+
+    console.log("");
+    console.log("========================================");
+    console.log("        MINECRAFT SPAWN");
+    console.log("========================================");
+    console.log("");
+
+    console.log(
+      "[MC] Minecraft Welt geladen."
+    );
+
+    if (
+      bot.entity &&
+      bot.entity.position
+    ) {
+
+      console.log(
+        "[MC] Position:",
+        bot.entity.position
+      );
+    }
+
+    console.log(
+      "[MC] AFK Bot kann jetzt weiterarbeiten."
+    );
+  }
+);
+
+bot.on(
+  "messagestr",
+  message => {
+
+    console.log(
+      "[CHAT] " + message
+    );
+  }
+);
+
+bot.on(
+  "kicked",
+  reason => {
+
+    console.log("");
+    console.log("========================================");
+    console.log("        MINECRAFT GEKICKT");
+    console.log("========================================");
+    console.log("");
+
+    console.log(
+      reason
+    );
+  }
+);
+
+bot.on(
+  "error",
+  error => {
+
+    console.log("");
+    console.log("========================================");
+    console.log("        MINECRAFT FEHLER");
+    console.log("========================================");
+    console.log("");
+
+    console.error(
+      error
+    );
+  }
+);
+
+bot.on(
+  "end",
+  () => {
+
+    console.log("");
+    console.log("========================================");
+    console.log("        MINECRAFT VERBINDUNG ENDE");
+    console.log("========================================");
+    console.log("");
+
+    console.log(
+      "[MC] Die Verbindung wurde beendet."
+    );
+  }
+);
+
+bot.on(
+  "death",
+  () => {
+
+    console.log(
+      "[MC] Bot ist gestorben."
+    );
+  }
+);
+
+bot.on(
+  "health",
+  () => {
+
+    if (
+      bot.health !== undefined &&
+      bot.food !== undefined
+    ) {
+
+      console.log(
+        "[MC] Leben:",
+        bot.health,
+        "| Essen:",
+        bot.food
+      );
+    }
+  }
+);
+
+process.on(
+  "uncaughtException",
+  error => {
+
+    console.log("");
+    console.log("========================================");
+    console.log("        UNCAUGHT EXCEPTION");
+    console.log("========================================");
+    console.log("");
+
+    console.error(
+      error
+    );
+  }
+);
+
+process.on(
+  "unhandledRejection",
+  error => {
+
+    console.log("");
+    console.log("========================================");
+    console.log("        UNHANDLED REJECTION");
+    console.log("========================================");
+    console.log("");
+
+    console.error(
+      error
+    );
+  }
+);
+
+process.on(
+  "SIGTERM",
+  () => {
+
+    console.log(
+      "[MC] SIGTERM erhalten."
+    );
+
+    try {
+
+      if (
+        bot
+      ) {
+
+        bot.quit(
+          "AFK Bot wird beendet"
+        );
+      }
+
+    } catch (
+      error
+    ) {
+
+      console.error(
+        error
+      );
+    }
+
+    setTimeout(
+      () => {
+        process.exit(0);
+      },
+      1000
+    );
+  }
+);
+
+process.on(
+  "SIGINT",
+  () => {
+
+    console.log(
+      "[MC] SIGINT erhalten."
+    );
+
+    try {
+
+      if (
+        bot
+      ) {
+
+        bot.quit(
+          "AFK Bot wird beendet"
+        );
+      }
+
+    } catch (
+      error
+    ) {
+
+      console.error(
+        error
+      );
+    }
+
+    setTimeout(
+      () => {
+        process.exit(0);
+      },
+      1000
+    );
+  }
+);
